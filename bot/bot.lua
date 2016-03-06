c2df7fee6a268a8acb7cbb7b98fd4b0e67b70baa
UniverseMelowdi <UniverseMelowdi@gmail.com>
2016-03-06 23:36:57 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -219,28 +219,13 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {67559249,71951412,0,tonumber(our_id)},--Sudo users
+    sudo_users = {137984481},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
 ]],
     help_text = [[
 Commands list :
